882860b9b9edfcc71ff4fea5a729e4bdab9af603
wut5238 <setthawut9915@hotmail.com>
2021-03-13 11:10:56 +0000

    modify hw_week6.py

--- hw_week6.py
+++ hw_week6.py
@@ -14,7 +14,7 @@ def ls_room():
     json_data = requests.get(url_list_room, headers=headers).json()
     print("list of rooms:")
     for i in json_data["items"]:
-        print(i["title"])
+        print("-", i["title"])
 ls_room()
 
 check = input("what room? ")
