de9b2769aaaf2383e10f2e338c653a89b977be7e
wut5238 <setthawut9915@hotmail.com>
2021-03-15 13:59:06 +0000

    modify output

--- hw_week6_interesting_API.py
+++ hw_week6_interesting_API.py
@@ -3,16 +3,17 @@ import urllib.parse
 import json
 from datetime import datetime
 
-country = input("Choose country which you want : ").lower()
+country = input("Enter country which you want (plese enter full name) : ").lower()
 url_ls_country = "https://restcountries.eu/rest/v2/all"
 json_data_lsc = requests.get(url_ls_country).json()
 
 for i in range(len(json_data_lsc)):
-    if country in json_data_lsc[i]["name"].lower():
-        url_currency_code = "https://restcountries.eu/rest/v2/name/"+country+"?fullText=true"
+    # print(country, json_data_lsc[i]["name"].lower())
+    if country == json_data_lsc[i]["name"].lower():
+        url_currency_code = "https://restcountries.eu/rest/v2/name/"+country
         json_data_cc = requests.get(url_currency_code).json()
         currency = json_data_cc[0]["currencies"][0]["code"]
-        print(currency)
+        print("currency code of {} is".format(json_data_lsc[i]["name"]), currency)
 
         url_rates = "https://api.exchangerate-api.com/v4/latest/USD"
         json_data_r = requests.get(url_rates).json()
